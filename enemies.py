b0617a799977b6edf1937c97be17e75814cdd384
Semen <demonica2003@mail.ru>
2020-01-08 02:31:11 +0700

    Полное движение врагов.

--- enemies.py
+++ enemies.py
@@ -12,31 +12,34 @@ class Enemy(pygame.sprite.Sprite):
         self.board = board
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.board.start_pos
-        self.passed_cells = []
+        self.passed_cells = set()
 
     def update(self):
-        done = False
-        if self.rect.x // 30 + 1 < len(self.board.board[0]):
-            if self.board.board[(self.rect.y - self.board.offset[1]) // 30][self.rect.x // 30 + 1] == "0":
-                self.rect.move(1, 0)
-                self.rect.x += 1
-                self.passed_cells.append(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
-                done = True
-            if 0 <= self.rect.x // 30 - 1:
-                if self.board.board[(self.rect.y - self.board.offset[1]) // 30][self.rect.x // 30 - 1] == "0" and not \
-                        (((self.rect.y - self.board.offset[1]) // 30), self.rect.x // 30 - 1) in self.passed_cells:
-                    self.rect.move(-1, 0)
-                    self.rect.x -= 1
-                    self.passed_cells.append(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
-                    done = True
-        if (self.rect.y - self.board.offset[1]) // 30 + 1 < len(self.board.board) and not done:
-            if self.board.board[(self.rect.y - self.board.offset[1]) // 30 + 1][self.rect.x // 30] == "0":
-                self.rect.move(0, 1)
-                self.rect.y += 1
-                self.passed_cells.append(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
-            if 0 <= (self.rect.y - self.board.offset[1]) // 30 - 1:
-                if self.board.board[(self.rect.y - self.board.offset[1]) // 30 - 1][self.rect.x // 30] == "0" and not \
-                        ((self.rect.y - self.board.offset[1]) // 30 - 1, self.rect.x // 30) in self.passed_cells:
-                    self.rect.move(0, -1)
-                    self.rect.y -= 1
-                    self.passed_cells.append(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
+        if self.rect.x // 30 + 1 < len(self.board.board[0]) and \
+                self.board.board[(self.rect.y - self.board.offset[1]) // 30][self.rect.x // 30 + 1] == "0" and not \
+                ((self.rect.y - self.board.offset[1]) // 30 , self.rect.x // 30 + 1) in self.passed_cells:
+            print("->")
+            self.rect.move(30, 0)
+            self.rect.x += 30
+            self.passed_cells.add(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
+        elif (self.rect.y - self.board.offset[1]) // 30 + 1 < len(self.board.board) and \
+                self.board.board[(self.rect.y - self.board.offset[1]) // 30 + 1][self.rect.x // 30] == "0" and not \
+                ((self.rect.y - self.board.offset[1]) // 30 + 1, self.rect.x // 30) in self.passed_cells:
+            print("|")
+            self.rect.move(0, 30)
+            self.rect.y += 30
+            self.passed_cells.add(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
+        elif 0 <= self.rect.x // 30 - 1 and self.board.board[(self.rect.y - self.board.offset[1]) // 30][
+            self.rect.x // 30 - 1] == "0" and not \
+                (((self.rect.y - self.board.offset[1]) // 30), self.rect.x // 30 - 1) in self.passed_cells:
+            print("<-")
+            self.rect.move(-30, 0)
+            self.rect.x -= 30
+            self.passed_cells.add(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
+        elif 0 <= (self.rect.y - self.board.offset[1]) // 30 - 1 and \
+                self.board.board[(self.rect.y - self.board.offset[1]) // 30 - 1][self.rect.x // 30] == "0" and not \
+                ((self.rect.y - self.board.offset[1]) // 30 - 1, self.rect.x // 30) in self.passed_cells:
+            print("^")
+            self.rect.move(0, -30)
+            self.rect.y -= 30
+            self.passed_cells.add(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
