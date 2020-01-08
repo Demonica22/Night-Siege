c51f1d4f4b825c57bbaab90da258a2d6508dc1a5
Semen <demonica2003@mail.ru>
2020-01-09 05:03:33 +0700

    Комментарии

--- enemies.py
+++ enemies.py
@@ -12,10 +12,15 @@ class Enemy(pygame.sprite.Sprite):
         self.board = board
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.board.start_pos
-        self.passed_cells = set()
+        self.passed_cells = set()  # множество из координат клеток, которые монстр уже прошел
         self.hp = 5
 
     def update(self):
+        """
+        Проверяет возможность движения монстра в любом из 4 направлений, учитывая невозможность пути в клетку,
+        в которой монстр уже был.
+        :return: None
+        """
         self.speed = 30
         if self.rect.x // 30 + 1 < len(self.board.board[0]) and \
                 self.board.board[(self.rect.y - self.board.offset[1]) // 30][self.rect.x // 30 + 1] == "0" and not \
@@ -43,9 +48,22 @@ class Enemy(pygame.sprite.Sprite):
             self.passed_cells.add(((self.rect.y - self.board.offset[1]) // 30, self.rect.x // 30))
 
     def is_killed(self):
+        """
+        Проверка, был ли убит монстр
+        :return: bool
+        """
         if self.hp <= 0:
             return True
         return False
 
     def get_shoted(self, bullet_power):
+        """
+        Получение выстрела с силой bullet_power (количество отнимаемого хп) и проверка на смерть монстра
+        :param bullet_power: Сила выстрела (урон по монстру)
+        :return: bool
+        """
         self.hp -= bullet_power
+        if self.is_killed():
+            self.board.current_money += 1
+            return True
+        return False
