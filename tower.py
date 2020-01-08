9741a76285500a6b7988adde065294936d4fb9eb
LR <liberal.renard@gmail.com>
2020-01-08 16:13:23 +0500

    Добавил второй тип башни

--- tower.py
+++ tower.py
@@ -3,12 +3,26 @@ import pygame
 pygame.init()
 
 TOWER = pygame.image.load("data/башня.jpg")
-
+TOWERS = pygame.image.load("data/strongtower.jpg")
 
 class Tower(pygame.sprite.Sprite):
-    def __init__(self, group, board, coords):
+    def __init__(self, group, board):
         self.image = TOWER
         super().__init__(group)
         self.board = board
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = coords
+
+class TowerLite(Tower):
+    def __init__(self):
+        self.image = TOWER
+        super().__init__(group)
+        self.board = board
+        self.rect = self.image.get_rect()
+
+
+class TowerStrong(Tower):
+    def __init__(self):
+        image_s = TOWERS
+        super().__init__(group)
+        self.board = board
+        self.rect = self.image_s.get_rect()
