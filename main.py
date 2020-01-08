0832303cc49f34f219173ef3b65c28e7c66c7bd0
Semen <demonica2003@mail.ru>
2020-01-09 05:04:22 +0700

    Добавлена атака монстров у башен; FIXED волны мобов.

--- main.py
+++ main.py
@@ -1,59 +1,9 @@
 import pygame
 from enemies import Enemy
 from tower import Tower, TowerStrong, TowerLite
+from board import Board
 
 pygame.init()
-
-WALL = pygame.image.load('data/wall.png')
-WALKWAY = pygame.image.load('data/walkway.jpg')
-LEVEL_ENDING = pygame.image.load('data/end.jpg')
-COINS = pygame.image.load('data/coins.png')
-START = pygame.image.load('data/start.jpg')
-TOWER = pygame.image.load('data/BigTower.jpg')
-STOWER = pygame.image.load('data/bigstrongtower.jpg')
-
-
-class Board:
-    def __init__(self, width, height, board, screen):
-        self.cell_size = 30
-        self.screen = screen
-        self.offset = (0, 60)
-        pygame.display.set_mode((width * 30, height * 30 + self.offset[1]))
-        self.width = width * self.cell_size
-        self.height = height * self.cell_size
-        self.board = board
-        self.current_money = 13
-
-    def render(self):
-        for elem in range(len(self.board)):
-            for cell in range(len(self.board[elem])):
-                x = cell * self.cell_size
-                y = self.offset[1] + elem * self.cell_size
-                if self.board[elem][cell] == "#":
-                    self.screen.blit(WALL, (x, y))
-                elif self.board[elem][cell] == "0":
-                    self.screen.blit(WALKWAY, (x, y))
-                elif self.board[elem][cell] == "X":
-                    self.screen.blit(LEVEL_ENDING, (x, y))
-                elif self.board[elem][cell] == "@":
-                    self.screen.blit(START, (x, y))
-                    self.start_pos = (x, y)
-                pygame.draw.rect(self.screen, (0, 0, 0), (x, y, self.cell_size, self.cell_size), 1)
-        self.screen.blit(COINS, (self.width - COINS.get_width(), 0))
-        self.screen.blit(TOWER, (0, 0))
-        self.screen.blit(STOWER, (60, 0))
-        font = pygame.font.Font(None, 30)
-        text = font.render("5", 1, (100, 255, 100))
-        screen.blit(text, (15, 40))
-        textstrong = font.render("10", 1, (100, 255, 100))
-        screen.blit(textstrong, (67, 40))
-        moneytext = font.render(str(self.current_money), 1, (100, 255, 100))
-        screen.blit(moneytext, (self.width - (COINS.get_width() // 1.2), 40))
-
-    def clicked(self, x, y):
-        if self.offset[0] <= x <= self.offset[0] + self.width:
-            if self.offset[1] <= y <= self.offset[1] + self.height:
-                return True
 
 
 def scan_level(level_file_name):
@@ -68,24 +18,24 @@ running = True
 screen.fill((0, 0, 0))
 board.render()
 all_enemies = pygame.sprite.Group()
-enemy = Enemy(all_enemies, board)
 all_towers = pygame.sprite.Group()
 clock = pygame.time.Clock()
 pygame.display.flip()
-current_wave = 1
 current_time = 0
+enemies_left = board.current_wave * 5
 tower1 = False
 tower2 = False
 while running:
     current_time += 0.5
-    if len(all_enemies) < current_wave * 5 and current_time % 12 == 0:
+    if enemies_left != 0 and current_time % 6 == 0:
+        enemies_left -= 1
         enemy = Enemy(all_enemies, board)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = event.pos
-            if not all([tower1, tower2]):
+            if not any([tower1, tower2]):
                 if 0 <= pos[0] <= 40 and 0 <= pos[1] <= 40 and board.current_money >= 5:
                     board.current_money -= 5
                     tower1 = True
@@ -107,11 +57,14 @@ while running:
     all_enemies.draw(screen)
     all_enemies.update()
     all_towers.draw(screen)
+    for tower in all_towers.sprites():
+        tower.attack(all_enemies.sprites())
     for enemy in all_enemies:
         if enemy.is_killed():
             all_enemies.remove(enemy)
-    if all([enemy.is_killed() for enemy in all_enemies]):
-        current_wave += 1
+    if enemies_left == 0 and not all_enemies.sprites():
+        board.current_wave += 1
+        enemies_left = board.current_wave * 5
         all_enemies = pygame.sprite.Group()
     pygame.display.flip()
     clock.tick(2)
