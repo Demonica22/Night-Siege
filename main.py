f400492cf066aa71c6e730133137b7a84b20bd67
Semen <demonica2003@mail.ru>
2020-01-09 17:49:22 +0700

    Переработана система работы с башнями; Добавлено улучшение башен (клик ПКМ).

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from enemies import Enemy
-from tower import Tower, TowerStrong, TowerLite
+from tower import Tower, IceTower, FireTower
 from board import Board
 
 pygame.init()
@@ -15,7 +15,7 @@ def scan_level(level_file_name):
 screen = pygame.display.set_mode((600, 600))
 board = Board(*scan_level("new_level"), screen)
 running = True
-screen.fill((0, 0, 0))
+screen.fill((71, 45, 23))
 board.render()
 all_enemies = pygame.sprite.Group()
 all_towers = pygame.sprite.Group()
@@ -23,8 +23,7 @@ clock = pygame.time.Clock()
 pygame.display.flip()
 current_time = 0
 enemies_left = board.current_wave * 5
-tower1 = False
-tower2 = False
+hand = False
 while running:
     current_time += 0.5
     if enemies_left != 0 and current_time % 6 == 0:
@@ -35,24 +34,31 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = event.pos
-            if not any([tower1, tower2]):
-                if 0 <= pos[0] <= 40 and 0 <= pos[1] <= 40 and board.current_money >= 5:
-                    board.current_money -= 5
-                    tower1 = True
-                elif 60 <= pos[0] <= 100 and 0 <= pos[1] <= 40 and board.current_money >= 10:
-                    board.current_money -= 10
-                    tower2 = True
-            if board.clicked(pos[0], pos[1]) and \
-                    board.board[(pos[1] - board.offset[1]) // 30][(pos[0] - board.offset[0]) // 30] == '#':
-                if tower1:
-                    tower = Tower(all_towers, board, (pos[0] // 30 * 30, pos[1] // 30 * 30))
-                    tower1 = False
-                    board.board[(pos[1] - board.offset[1]) // 30][(pos[0] - board.offset[0]) // 30] = '1'
-                elif tower2:
-                    tower = TowerStrong(all_towers, board, (pos[0] // 30 * 30, pos[1] // 30 * 30))
-                    tower2 = False
-                    board.board[(pos[1] - board.offset[1]) // 30][(pos[0] - board.offset[0]) // 30] = '2'
-    screen.fill((0, 0, 0))
+            if event.button == 1:
+
+                if not hand:
+                    if 0 <= pos[0] <= 40 and 0 <= pos[1] <= 40 and board.current_money >= 5:
+                        board.current_money -= 5
+                        hand = "fire"
+                    elif 60 <= pos[0] <= 100 and 0 <= pos[1] <= 40 and board.current_money >= 10:
+                        board.current_money -= 10
+                        hand = "ice"
+                if board.clicked(pos[0], pos[1]) and \
+                        board.board[(pos[1] - board.offset[1]) // 30][(pos[0] - board.offset[0]) // 30] == '#':
+                    if hand:
+                        if hand == "fire":
+                            tower = FireTower(all_towers, board, (pos[0] // 30 * 30, pos[1] // 30 * 30))
+                            board.board[(pos[1] - board.offset[1]) // 30][(pos[0] - board.offset[0]) // 30] = 'F'
+                        elif hand == "ice":
+                            tower = IceTower(all_towers, board, (pos[0] // 30 * 30, pos[1] // 30 * 30))
+                            board.board[(pos[1] - board.offset[1]) // 30][(pos[0] - board.offset[0]) // 30] = '2'
+                        hand = False
+            if event.button == 3:
+                for tower in all_towers.sprites():
+                    if tower.clicked(pos[0], pos[1]):
+                        print(tower.upgrade())
+
+    screen.fill((71, 45, 23))
     board.render()
     all_enemies.draw(screen)
     all_enemies.update()
--- tower.py
+++ tower.py
@@ -2,21 +2,15 @@ import pygame
 
 pygame.init()
 
-TOWER = pygame.image.load("data/башня.jpg")
-TOWERS = pygame.image.load("data/strongtower.jpg")
-
 
 class Tower(pygame.sprite.Sprite):
     def __init__(self, group, board, coords):
         super().__init__(group)
-        self.image = TOWER
-        self.rect = self.image.get_rect()
         self.board = board
-        self.rect.x, self.rect.y = coords
         self.range = 1  # дальность атаки башни
         self.power = 1
 
-    def click(self, x, y):
+    def clicked(self, x, y):
         if x <= self.x + self.image.get_width() and self.x >= x:
             if y <= self.y + self.image.get_height() and self.y >= y:
                 return True
@@ -25,31 +19,97 @@ class Tower(pygame.sprite.Sprite):
     def attack(self, enemies):
         if enemies:
             for target in enemies.copy():
-                if self.rect.x - self.range * self.board.cell_size <= target.rect.x <= self.rect.x + self.range * self.board.cell_size and \
-                        self.rect.y - self.range * self.board.cell_size <= target.rect.y <= self.rect.y + self.range * self.board.cell_size:
+                if self.rect.x - self.range * self.board.cell_size <= target.rect.x <= self.rect.x + \
+                        self.range * self.board.cell_size and \
+                        self.rect.y - self.range * self.board.cell_size <= target.rect.y <= self.rect.y + \
+                        self.range * self.board.cell_size:
                     if target.get_shoted(self.power):
                         enemies.remove(target)
                 else:
                     enemies.remove(target)
 
 
-class TowerLite(Tower):
+class FireTower(Tower):
     def __init__(self, group, board, coords):
         super().__init__(group, board, coords)
-        self.image = TOWER
+        self.level = 1
+        self.images = [pygame.image.load("data/tower" + str(n) + ".png").convert_alpha() for n in range(1, 4)]
+        # Список изображений башен по уровням
+        self.image = self.images[self.level - 1]
         self.board = board
         self.range = 1
         self.power = 1
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = coords
+        self.upgrade_cost = [5, 10, 15]  # [level1, level2, level3]
+
+    def upgrade(self):
+        """
+        Функция првоеряет, может ли быть башня улучшена, если может то улучшает и изменяет board.current_money,
+        self.level, self.power
+        :return: str ( результат)
+        """
+        if self.level < 3:
+            if self.board.current_money >= self.upgrade_cost[self.level]:
+                self.board.current_money -= self.upgrade_cost[self.level]
+                self.level += 1
+                self.image = self.images[self.level - 1]
+                self.power *= 2
+                return "UPGRADED"
+            return "Not enough money"
+        return "max level"
+
+    def clicked(self, x, y):
+        """
+        Был ли клик на Башню
+        :param x: int
+        :param y: int
+        :return: bool
+        """
+        if self.rect.x <= x <= self.rect.x + self.image.get_width():
+            if self.rect.y <= y <= self.rect.y + self.image.get_height():
+                return True
+        return False
 
 
-class TowerStrong(Tower):
+class IceTower(Tower):
     def __init__(self, group, board, coords):
         super().__init__(group, board, coords)
-        self.range = 2
-        self.power = 5
-        self.image = TOWERS
+        self.level = 1
+        self.images = [pygame.image.load("data/tower" + str(n) + ".png").convert_alpha() for n in range(4, 7)]
+        # Список изображений башен по уровням
+        self.image = self.images[self.level - 1]
+        self.range = 3
+        self.power = 2
         self.board = board
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = coords
+        self.upgrade_cost = [10, 20, 30]  # [level1, level2, level3]
+
+    def upgrade(self):
+        """
+        Функция првоеряет, может ли быть башня улучшена, если может то улучшает и изменяет board.current_money,
+        self.level, self.power
+        :return: str ( результат)
+        """
+        if self.level < 3:
+            if self.board.current_money >= self.upgrade_cost[self.level]:
+                self.board.current_money -= self.upgrade_cost[self.level]
+                self.level += 1
+                self.power *= 2
+                self.image = self.images[self.level - 1]
+                return "UPGRADED"
+            return "Not enough money"
+        return "max level"
+
+    def clicked(self, x, y):
+        """
+        Был ли клик на Башню
+        :param x: int
+        :param y: int
+        :return: bool
+        """
+        if self.rect.x <= x <= self.rect.x + self.image.get_width():
+            if self.rect.y <= y <= self.rect.y + self.image.get_height():
+                return True
+        return False
